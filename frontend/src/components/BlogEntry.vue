30fe78d5e15f9ddf22496f6329f9606de07d09c9
Yevhenii Shudrenko <yevhenii.shudrenko@tuhh.de>
2023-04-25 19:37:06 +0200

    Add icons

--- frontend/src/components/BlogEntry.vue
+++ frontend/src/components/BlogEntry.vue
@@ -3,7 +3,8 @@ import { ref } from 'vue'
 
 const props = defineProps({
   description: String,
-  imageUrl: String
+  imageUrl: String,
+  isLiked: Boolean
 })
 
 </script>
@@ -18,6 +19,10 @@ const props = defineProps({
               cover
               :src=imageUrl
             ></v-img>
+            <div class="mt-2 d-inline-flex justify-end border w-100">
+              <v-icon icon="mdi-heart" v-if="isLiked"></v-icon>
+              <v-icon icon="mdi-heart-outline" v-else></v-icon>
+            </div>
             <!-- <div class="mt-2">{{ description }}</div> -->
             <div class="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
 
