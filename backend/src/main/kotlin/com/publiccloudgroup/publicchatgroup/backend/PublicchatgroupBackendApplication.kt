da8066d8b027e906a906f31ce0ecc870d030f40f
Marcel Mundl <marcel@mundlhome.de>
2023-04-25 15:48:01 +0200

    initial commit

---
+++ backend/src/main/kotlin/com/publiccloudgroup/publicchatgroup/backend/PublicchatgroupBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.publiccloudgroup.publicchatgroup.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PublicchatgroupBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<PublicchatgroupBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/publiccloudgroup/publicchatgroup/backend/PublicchatgroupBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.publiccloudgroup.publicchatgroup.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PublicchatgroupBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
