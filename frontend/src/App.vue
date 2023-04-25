f84f2c3576c05e3c579095b1479be0bf70fcf132
Yevhenii Shudrenko <yevhenii.shudrenko@tuhh.de>
2023-04-25 17:10:12 +0200

    Add another example-layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,128 @@
 <script setup>
 import BlogEntry from './components/BlogEntry.vue';
 import HelloWorld from './components/HelloWorld.vue'
+
+import { ref, reactive } from 'vue'
+
+const drawer = ref('');
+const cards = reactive(['Today', 'Yesterday']);
+const links = reactive([
+        ['mdi-inbox-arrow-down', 'Inbox'],
+        ['mdi-send', 'Send'],
+        ['mdi-delete', 'Trash'],
+        ['mdi-alert-octagon', 'Spam'],
+      ]);
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <BlogEntry />
+    <!-- <v-system-bar>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet
+        color="grey-lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey-darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          link
+        >
+          <template v-slot:prepend>
+            <v-icon>{{ icon }}</v-icon>
+          </template>
+
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container
+        class="py-8 px-6"
+        fluid
+      >
+        <v-row>
+          <v-col
+            v-for="card in cards"
+            :key="card"
+            cols="12"
+          >
+            <v-card>
+
+              <v-list lines="two">
+                <v-list-subheader>{{ card }}</v-list-subheader>
+                <template v-for="n in 6" :key="n">
+                  <v-list-item>
+                    <template v-slot:prepend>
+                      <v-avatar color="grey-darken-1"></v-avatar>
+                    </template>
+
+                    <v-list-item-title>Message {{ n }}</v-list-item-title>
+
+                    <v-list-item-subtitle>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                    </v-list-item-subtitle>
+                  </v-list-item>
+
+                  <v-divider
+                    v-if="n !== 6"
+                    :key="`divider-${n}`"
+                    inset
+                  ></v-divider>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main> -->
+
+    <!-- <v-container>
+      <v-row>
+        <v-col cols="2" class="border">Sidebar</v-col>
+        <v-col cols="10">
+          Content
+          <BlogEntry />
+        </v-col>
+      </v-row>
+    </v-container> -->
+
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer>
+          <v-list>
+            <v-list-item title="Navigation drawer"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar title="Application bar"></v-app-bar>
+
+        <v-main style="min-height: 300px;"></v-main>
+      </v-layout>
+    </v-card>
+  
 </template>
 
 <style scoped>
