f2cf75bed678a7557787136e8b256ef6ee7dd3c1
YasinAkimura <lytek@hotmail.de>
2023-04-25 19:33:29 +0200

    app bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,15 @@
 
 <template>
     <div class="layout">
-      <div class="sidebar">Sidebar</div>
+      <div class="sidebar">
+        <v-toolbar :color="'#4c70a2'">
+          <h2 class="bar_title">Beitrag posten</h2>
+        </v-toolbar>
+        <div class="input_area">
+          <v-textarea label="Storie" auto-grow variant="solo"></v-textarea>
+        </div>
+        <v-btn class="m8">Posten</v-btn>
+      </div>
       <v-col cols="10">
         Content
         <BlogEntry />
@@ -17,18 +25,35 @@
   flex:1;
   display: flex;
   flex-direction: row;
+  background-color: hsl(250,100%,99%);
 }
 .sidebar {
-  width:70rem;
   flex:1;
+  width:70rem;
+  flex-direction: column;
+  background-color: #fff;
   box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.19);
   display: flex;
+}
+.input_area{
+  min-height:80px;
+  margin:8px;
+  position: relative;
+}
+.bar_title {
+  padding-left: 12px;
+  font-size: 20px;
+  font-weight: 600;
+  color:#fff;
 }
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+}
+.m8 {
+  margin:8px;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
